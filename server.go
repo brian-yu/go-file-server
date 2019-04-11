6dda2bf48a2b1bcf84623589a074583daf7b0859
ThaumicMekanism <thaumicmekanism@gmail.com>
2019-04-11 10:02:01 -0700

    Add comments to the code

--- server.go
+++ server.go
@@ -15,29 +15,47 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	// Note that we will be using userlib.ReadFile we provided to read files on the system.
 	// The path to the file is given by r.URL.Path and will be the path to the string.
 	// Make sure you properly sanitise it (more described in get file).
+	/*** YOUR CODE HERE ***/
+	// Reads the file from the disk
 	response, err := userlib.ReadFile(r.URL.Path[1:])
 	if err != nil {
+		// If we have an error from the read, will return the generic file error message and set the error code to follow that.
 		http.Error(w, userlib.FILEERRORMSG, userlib.FILEERRORCODE)
 		return
 	}
+	/*** YOUR CODE HERE END ***/
 
 	// This will automagically set the right content type for the
 	// reply as well.
+	// We need to set the correct header code for a success since we should only succeed at this point.
 	w.WriteHeader(userlib.SUCCESSCODE) // Make sure you write the correct header code so that the tests do not fail!
+	// Write the data which is given to us by the response.
 	w.Write(response)
 }
 
+// This function will handle the requests to acquire the cache status.
 func cacheHandler(w http.ResponseWriter, r *http.Request) {
+	// Sets the header of the request to a plain text format since we are just dumping information about the cache.
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(userlib.SUCCESSCODE) // Make sure you write the correct header code so that the tests do not fail!
+	// Set the success code to the proper success code since the action should not fail.
+	w.WriteHeader(userlib.SUCCESSCODE)
+	// Get the cache status string from the getCacheStatus function.
 	w.Write([]byte(getCacheStatus()))
+}
+
+// This function will handle the requests to clear/restart the cache.
+func cacheClearHandler(w http.ResponseWriter, r *http.Request) {
+	// Sets the header of the request to a plain text format since we are just dumping information about the cache.
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	// Set the success code to the proper success code since the action should not fail.
+	w.WriteHeader(userlib.SUCCESSCODE)
+	// Get the cache status string from the getCacheStatus function.
+	w.Write([]byte(CacheClear()))
 }
 
 // The structure used for responding to file requests.
 // It contains the file contents (if there is any)
 // or the error returned when accessing the file.
-// You can add fields in here so long as you do not 
-// change or remove the existing fields.
 type fileResponse struct {
 	responseData []byte
 	responseError error
@@ -48,17 +66,22 @@ type fileResponse struct {
 // information.
 type fileRequest struct {
 	filename string
-	response chan(*fileResponse)
+	response chan *fileResponse
 }
 
+// Port of the server to run on
 var port int
+// Capacity of the cache in Bytes
 var capacity int
+// Timeout for file reads in Seconds.
 var timeout int
 
-// These names must not change or you will not pass any of the autograder tests!
-var fileChan chan(*fileRequest) = make(chan(*fileRequest))
-var cacheCapacityChan chan(chan(string)) = make(chan(chan(string)))
-var cacheCloseChan chan(chan(string)) = make(chan(chan(string)))
+// The channel to pass file read requests to. This is how you will get a file from the cache.
+var fileChan = make(chan *fileRequest )
+// The channel to pass a request to get back the capacity info of the cache.
+var cacheCapacityChan = make(chan chan string)
+// The channel where a bool passed into it will cause the OperateCache function to be closed and all of the data to be cleared.
+var cacheCloseChan = make(chan bool)
 
 // A wrapper function that does the actual getting of the file
 func getFile(filename string) (response *fileResponse) {
@@ -73,16 +96,35 @@ func getFile(filename string) (response *fileResponse) {
 	// You should also return a file not found error if after `timeout`
 	// seconds if there is no response from the cache, so you will
 	// need to modify the end of the function as well.
-	
+	/*** YOUR CODE HERE ***/
+
+	/*** YOUR CODE HERE END ***/
+
+	// Makes the file request object.
 	request := fileRequest{filename, make(chan(*fileResponse))}
+	// Sends a pointer to the file request object to the fileChan so the cache can process the file request.
 	fileChan <- &request
+	// Returns the result (from the fileResponse channel)
 	return <- request.response
 }
 
 func getCacheStatus() (response string) {
-	responseChan := make(chan(string))
+	// Make a channel for the response of the Capacity request.
+	responseChan := make(chan string)
+	// Send the response channel to the capacity request channel.
 	cacheCapacityChan <- responseChan
+	// Return the reply.
 	return <- responseChan
+}
+
+func CacheClear() (response string) {
+	// Send the response channel to the capacity request channel.
+	cacheCloseChan <- true
+	// We should only return to here once we are sure the currently open cache will not process any more requests.
+	// This is because the close channel is blocking until it pulls the item out of there.
+	// Now that the cache should be closed, lets relaunch the cache.
+	go operateCache()
+	return userlib.CacheCloseMessage
 }
 
 
@@ -96,28 +138,29 @@ type cacheEntry struct {
 // This function is where you need to do all the work...
 // Basically, you need to...
 
-
-
-// 1:  Create a map to store all the cache entries.  Your cache
-// entry stru
+// 1:  Create a map to store all the cache entries.
 
 // 2:  Go into a continual select loop.
 
 // Hint, you are going to want another channel 
 
 func operateCache() {
+	/*** YOUR CODE HERE ***/
+	// Init our cache to zero bytes.
 	currentCapacity := 0
-	currentCapacity = currentCapacity
-	fileMap := make(map[string] *fileResponse)
-	fileMap = fileMap
+	currentCapacity = currentCapacity // This is just to prevent Golang from yelling at us about unused variables. Removed this once you use the variable elsewhere.
+	// Make a file map (this is just like a hashmap in java) for the cache entries.
+	fileMap := make(map[string] *cacheEntry)
+	fileMap = fileMap // This is just to prevent Golang from yelling at us about unused variables. Removed this once you use the variable elsewhere.
 	for {
+		// We want to select what we want to do based on what is in different cache channels.
 		select {
 		case fileReq := <- fileChan:
-			fileReq = fileReq
+			fileReq = fileReq // This is just to prevent Golang from yelling at us about unused variables. Removed this once you use the variable elsewhere.
 			// Handle a file request here.
 
 		case cacheReq := <- cacheCapacityChan:
-			cacheReq = cacheReq
+			cacheReq = cacheReq // This is just to prevent Golang from yelling at us about unused variables. Removed this once you use the variable elsewhere.
 			// Handle a cache capacity request here.
 
 		case <- cacheCloseChan:
@@ -125,25 +168,31 @@ func operateCache() {
 			// Make sure you clean up all of your cache state or you will fail most all of the tests!
 			return
 		}
-
 	}
-
+	/*** YOUR CODE HERE END ***/
 }
 
 
 func main(){
+	// Initialize the arguments when the main function is ran. This is to setup the settings needed by
+	// other parts of the file server.
 	flag.IntVar(&port, "p", 8080, "Port to listen for HTTP requests (default port 8080).")
 	flag.IntVar(&capacity, "c", 100000, "Number of bytes to allow in the cache.")
 	flag.IntVar(&timeout, "t", 2, "Default timeout (in seconds) to wait before returning an error")
+	// Parse the args.
 	flag.Parse()
+	// Say that we are starting the server.
 	fmt.Printf("Server starting, port %v\n", port)
 	serverString := fmt.Sprintf(":%v", port)
 
+	// Set up the service handles for certain pattern requests in the url.
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/cache/", cacheHandler)
+	http.HandleFunc("/cache/clear/", cacheClearHandler)
 
 	// Start up the cache logic...
 	go operateCache()
-	
+
+	// This starts the web server and will cause it to continue to listen and respond to web requests.
 	log.Fatal(http.ListenAndServe(serverString, nil))
 }
